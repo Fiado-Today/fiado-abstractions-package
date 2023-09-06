fbd111c58cbc1b6a238cb1cbd66e2ddfbccd62e3
Jose Aguirre <jma@somosfiado.com>
2023-09-06 13:43:23 -0500

    Actualizacion de la paqueteria para beneficiarios

--- src/Fiado/Account/Beneficiary/dto/CreateBeneficiaryInput.ts
+++ src/Fiado/Account/Beneficiary/dto/CreateBeneficiaryInput.ts
@@ -1,8 +1,5 @@
 import {Beneficiary} from "../types/Beneficiary";
-import {TypeOfDirectoryIdEnum} from "../../../../General/enum/TypeOfDirectoryIdEnum";
 
 export interface CreateBeneficiaryInput {
-    beneficiary: Beneficiary,
-    directoryId: string,
-    typeOfDirectoryId: TypeOfDirectoryIdEnum
+    beneficiary: Beneficiary
 }
