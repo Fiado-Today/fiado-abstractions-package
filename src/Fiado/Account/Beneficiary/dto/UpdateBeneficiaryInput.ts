888cb3f48a282afb0cba46fcb1c2a540a5afd0e2
Jose Aguirre <jma@somosfiado.com>
2023-09-07 15:51:52 -0500

    Ajuste DTO's de beneficiarios

--- src/Fiado/Account/Beneficiary/dto/UpdateBeneficiaryInput.ts
+++ src/Fiado/Account/Beneficiary/dto/UpdateBeneficiaryInput.ts
@@ -1,8 +1,11 @@
 import {Address} from "../../../../KYC/types/Address";
+import {TypeOfDirectoryIdEnum} from "../../../../General/enum/TypeOfDirectoryIdEnum";
 
 export interface UpdateBeneficiaryInput {
     fullName?: string;
     typeOfRelationshipId?: string;
     percentage?: number;
     address?: Address;
+    directoryId?: string;
+    typeOfDirectoryId?: TypeOfDirectoryIdEnum;
 }
