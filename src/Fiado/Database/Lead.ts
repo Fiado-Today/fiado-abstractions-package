697bd8601c0b04d2a23bb185e1ada588a54d3ec9
Jose Aguirre <jma@somosfiado.com>
2023-09-12 14:59:46 -0500

    Entidad BD para Lead

--- src/Fiado/Database/Lead.ts
+++ src/Fiado/Database/Lead.ts
@@ -4,14 +4,24 @@ import {TypeOfDirectoryIdEnum} from "../../General/enum/TypeOfDirectoryIdEnum";
 import {UserProfileEnum} from "../../General/enum/UserProfileEnum";
 
 export interface Lead {
-    id: string,
-    phoneNumber: string,
-    promotedToDirectory: boolean,
-    referralCode: string | null,
-    referralDirectoryId: string | null,
-    scope: UserScopeEnum[],
-    stateOfBirth: string | null,
-    tenantId: TenantEnum,
-    typeOfDirectoryId: TypeOfDirectoryIdEnum,
-    userProfile: UserProfileEnum,
+    id: string;
+    agentCode: string | null;
+    agentDirectoryId: string | null;
+    countryOfBirth: string | null;
+    createDate: string;
+    curp: string | null;
+    dateOfBirth: string | null;
+    eventId: string | null;
+    lastName: string;
+    name: string;
+    ownerDirectoryId: string | null;
+    phoneNumber: string;
+    promotedToDirectory: boolean;
+    referralCode: string | null;
+    referralDirectoryId: string | null;
+    scope: UserScopeEnum[];
+    stateOfBirth: string | null;
+    tenantId: TenantEnum;
+    typeOfDirectoryId: TypeOfDirectoryIdEnum;
+    userProfile: UserProfileEnum;
 }
