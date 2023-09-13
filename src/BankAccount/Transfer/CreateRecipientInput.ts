e1f6e8bba20b7996bbf5d20bfc50366aa82cad80
mssomosfiado <130305245+mssomosfiado@users.noreply.github.com>
2023-09-13 13:15:58 -0500

    arreglando objet de beneficiario

--- src/BankAccount/Transfer/CreateRecipientInput.ts
+++ src/BankAccount/Transfer/CreateRecipientInput.ts
@@ -1,7 +1,16 @@
-export interface CreateRecipientInput {
-    sender_user_id?: number;
-    program_id?: number;
-    transfer_method: number;
-    external_account_number: string;
-    external_routing_number: String;
+export interface CreateRecipientInput<T> {
+    firstName: string,
+    lastName: string,
+    externalRecipientId: string,
+    transferMethod: number,
+    externalAccountNumber: string,
+    externalRoutingNumber: string,
+    provinceId: number,
+    city: string,
+    address: string,
+    postal: string,
+    phoneNumber: string,
+    email: string,
+    iso_code: string,
+    currency: string
 }
