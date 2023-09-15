824803d35f5ba6da212cada4e4a9dabe6831e5c7
yhonhansen <ycolina@fiado.today>
2023-09-15 07:32:36 -0500

    fix: se ajusta proveedores de bancos

--- src/core/account/dtos/AccountOutput.ts
+++ src/core/account/dtos/AccountOutput.ts
@@ -1,5 +1,5 @@
+import { BankAccountProviders } from "../../../BankAccount/enums/BankAccountProviders"
 import { AccountStatus } from "../../../Fiado/Account/enums/AccountStatus"
-import { BankProvider } from "../../../General/enum/BankProvider"
 import { Country } from "../../../General/enum/Country"
 
 
@@ -8,7 +8,7 @@ export type AccountOutput = {
     typeOfAccountId: null,
     externalAccountId: string,
     externalSubAccountId: string,
-    provider: BankProvider,
+    provider: BankAccountProviders,
     directoryId: string,
     typeOfDirectoryId: string,
     currentBalance: number,
