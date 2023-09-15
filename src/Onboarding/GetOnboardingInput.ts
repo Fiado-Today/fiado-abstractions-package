5f2969e966d042ac9c4d18ef645e8ca0e6a7b977
egarc-fiado <96536085+egarc-fiado@users.noreply.github.com>
2023-09-15 10:23:19 -0500

    changes on get onboarding input

--- src/Onboarding/GetOnboardingInput.ts
+++ src/Onboarding/GetOnboardingInput.ts
@@ -1,4 +1,7 @@
+import { TypeOfDirectoryIdEnum } from "../General/enum/TypeOfDirectoryIdEnum";
+
 export interface GetOnboardingInput {
+    typeOfDirectoryId: TypeOfDirectoryIdEnum
     phoneNumber?: string;
     id?: string;
 }
