7f9ae912e4e14d5559cd3ac30024aac80eebe036
egarc-fiado <96536085+egarc-fiado@users.noreply.github.com>
2023-09-18 08:18:52 -0700

    update

--- src/OnboardingStep/GetOnboardingStepInput.ts
+++ src/OnboardingStep/GetOnboardingStepInput.ts
@@ -1,9 +1,10 @@
 import { Country } from "../General/enum/Country";
+import { TypeOfDirectoryIdEnum } from "../General/enum/TypeOfDirectoryIdEnum";
 
 export interface GetOnboardingStepInput {
     countryId: Country;
     directoryId: string;
-    typeOfDirectoryId:string
+    typeOfDirectoryId:TypeOfDirectoryIdEnum
 }
 
 
