17e10335c808632bd8492708710f4584ebaada30
mssomosfiado <130305245+mssomosfiado@users.noreply.github.com>
2023-09-21 16:44:03 -0500

    fix merge 1

--- src/Fiado/Bank/bankAccount/dto/GetBankAccountsListInput.ts
+++ src/Fiado/Bank/bankAccount/dto/GetBankAccountsListInput.ts
@@ -1,12 +1,12 @@
 export interface GetBankAccountsListInput {
-    countryId?: string,
-    bankAccountId?: string,
-    accountNumber?: string,
-    directoryId?: string,
-    typeOfDirectoryId?: string,
-    alias?: string,
-    accountTypeId?: string,
-    accountHolderName?: string,
-    bankId?: string
-    default?: boolean
+    countryId?: string;
+    bankAccountId?: string;
+    accountNumber?: string;
+    directoryId?: string;
+    typeOfDirectoryId?: string;
+    alias?: string;
+    accountTypeId?: string;
+    accountHolderName?: string;
+    bankId?: string;
+    default?: boolean;
 }
--- src/Fiado/Bank/bankAccount/dto/GetBankAccountsListOutput.ts
+++ src/Fiado/Bank/bankAccount/dto/GetBankAccountsListOutput.ts
@@ -1,15 +1,15 @@
 import {AccountType} from "../type/AccountType";
 export interface GetBankAccountsListOutput {
-    id: string,
-    countryId: string,
-    accountNumber: string | null,
-    bankId: string,
-    alias: string | null,
-    directoryId: string,
-    typeOfDirectoryId: string,
-    accountHolderName: string,
-    email: string | null,
-    accountType: [AccountType],
-    bankName: string | null,
-    iconUrl: string | null
+    id: string;
+    countryId: string;
+    accountNumber: string | null;
+    bankId: string;
+    alias: string | null;
+    directoryId: string;
+    typeOfDirectoryId: string;
+    accountHolderName: string;
+    email: string | null;
+    accountType: [AccountType];
+    bankName: string | null;
+    iconUrl: string | null;
 }
