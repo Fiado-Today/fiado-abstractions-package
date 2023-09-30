1ee362cd79a6993c71ba348fe12d42aa34c921bc
egarc-fiado <96536085+egarc-fiado@users.noreply.github.com>
2023-09-29 19:49:30 -0600

    v 165

--- src/Onboarding/GetOnboardingListInput.ts
+++ src/Onboarding/GetOnboardingListInput.ts
@@ -6,6 +6,7 @@ export interface GetOnboardingListInput extends GetListBaseInput {
     referralCode?:string | null;
     userProfile?:DirectoryProfileEnum;
     promotedToDirectory?:boolean;
+    myReferralCode?:string;
     typeOfDirectoryId:TypeOfDirectoryIdEnum;
     phoneNumber?:string;
     name?:string;
