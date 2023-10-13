8dd8bbb7b7429a98a7b5286767c29169a00abf78
yhonhansen <ycolina@fiado.today>
2023-10-13 07:13:30 -0500

    fix: nueva versión

--- src/BankAccount/User/CreateBankAccountUserInput.ts
+++ src/BankAccount/User/CreateBankAccountUserInput.ts
@@ -1,5 +1,4 @@
 import { AddressOutput } from "../../Fiado/Address/AddressOutput";
-import { TypeOfDocument } from "../../KYC/enums/TypeOfDocument";
 
 export interface CreateBankAccountUserInput {
     directoryId: string,
@@ -10,6 +9,5 @@ export interface CreateBankAccountUserInput {
     address: AddressOutput,
     email: string,
     dob: string,
-    typeOfDocumentId: TypeOfDocument
     documentNumber: string,
 }
