892abddd8732a5c08d8f452f66deee82a348e587
egarc-fiado <96536085+egarc-fiado@users.noreply.github.com>
2023-10-18 10:24:30 -0600

    v 224

--- src/Fiado/Database/EntitiesEnum.ts
+++ src/Fiado/Database/EntitiesEnum.ts
@@ -4,4 +4,7 @@ export enum EntitiesEnum {
     DIRECTORY_USER = "DirectoryUser",
     TRANSACTION_MESSAGE = "TransactionMessage",
     CREDIT_CARD_REQUEST = "CreditCardRequest",
+    CREDIT_CONTRACT = "CreditContract",
+    CV_CONTRACT = "CVContract",
+    COLLECTOR_TRANSACTION = "CollectorTransaction",
 }
