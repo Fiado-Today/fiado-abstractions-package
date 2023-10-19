e2b99c77a43aa4f82e30ee5a96f945a278d7cf46
yhonhansen <ycolina@fiado.today>
2023-10-19 17:11:23 -0500

    fix: se agrega indice creditCardRequest

---
+++ src/Fiado/Database/indexes/CreditCardRequestIndexList.ts
@@ -0,0 +1,11 @@
+import { EntitiesEnum } from "../EntitiesEnum";
+import { DatabaseIndexListType } from "./DatabaseIndexListType";
+
+export const CreateCreditCardRequestIndexList:DatabaseIndexListType= { 
+    OWNER_DIRECTORY_ID:{
+        schema: EntitiesEnum.CREDIT_CARD_REQUEST,
+        required: [":#ownerDirectoryId"],
+        indexName: "ownerDirectoryId-index",
+        keyCondition: "#ownerDirectoryId = :ownerDirectoryId"
+    }
+}
