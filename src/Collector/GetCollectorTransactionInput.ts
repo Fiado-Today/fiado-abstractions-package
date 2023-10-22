6f6194a402e523d94d7ba52cb6c7df1efaffa5ef
egarc-fiado <96536085+egarc-fiado@users.noreply.github.com>
2023-10-22 17:06:01 -0600

    v 235

--- src/Collector/GetCollectorTransactionInput.ts
+++ src/Collector/GetCollectorTransactionInput.ts
@@ -2,6 +2,8 @@ import { GetListBaseInput } from "../Fiado/Common/GetListBaseInput";
 
 export interface GetCollectorTransactionInput extends GetListBaseInput{
     transactionNumber?:string,
-    relatedIDTern?:string,
+    relatedExternalId?:string,
     directoryUserId?:string,
+    availableBalance?:boolean,
+    cobrado?:boolean
 }
