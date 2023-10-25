d3d3f0876bc5e94cc0660639d959b7592cb2e811
yhonhansen <ycolina@fiado.today>
2023-10-25 08:10:45 -0500

    fix: nueva versión - se agrega entity Card

---
+++ src/Fiado/Database/Card.ts
@@ -0,0 +1,38 @@
+import { BankAccountProviders } from "../../BankAccount/enums/BankAccountProviders";
+import { Country } from "../../General/enum/Country";
+import { Address } from "./Address";
+
+export type Card = {
+
+    id: string,
+    accountId: string,
+    activationValue: string,
+    address: Address,
+    countryId: Country,
+    createDate: string,
+    updateDate: string,
+    creditOrDebit: string,
+    directtoryId: string,
+    expirationMonth: string,
+    expirationYear: string,
+    externalCardId: string,
+    holder?: {
+        name: string,
+        lastName: string
+    },
+    imageUrl: string,
+    mii: string,
+    nominated: boolean,
+    pan: string,
+    panLastDigits: string,
+    provider: {
+        id: BankAccountProviders,
+        externalUserId?: string,
+        externalCardId: string,
+        externalAccountId?: string
+    },
+    status: string,
+    tenantId: string,
+    typeOfCardId: string,
+    typeOfDirectoryId: string,
+}
