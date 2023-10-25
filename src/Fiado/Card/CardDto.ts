f31171ab78ac76bf2ae1131c31fd051d018bbea6
yhonhansen <ycolina@fiado.today>
2023-10-25 12:15:13 -0500

    fix: nueva versión

---
+++ src/Fiado/Card/CardDto.ts
@@ -0,0 +1,18 @@
+
+
+export type CardDto = {
+
+    id: string,
+    mii: string,
+    pan: string,
+    cvc: string,
+    typeOfCardId: string,
+    creditOrDebit: string,
+    expirationMonth: string,
+    expirationYear: string,
+    status: string,
+    imageUrl: string,
+    countryId: string,
+    holderName: string,
+}
+
