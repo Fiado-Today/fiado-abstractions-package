4659ed90bf38953ee22f8aae7ae8e71fe7045e0d
egarc-fiado <96536085+egarc-fiado@users.noreply.github.com>
2023-11-10 16:27:31 -0600

    v 281

--- src/P2PContact/GetP2PTransactionCounterInput.ts
+++ src/P2PContact/GetP2PTransactionCounterInput.ts
@@ -1,4 +1,6 @@
-export interface GetP2PTransactionCounterInput {
+import { GetListBaseInput } from "../Fiado/Common/GetListBaseInput";
+
+export interface GetP2PTransactionCounterInput extends GetListBaseInput {
     fromDirectoryId?:string,
     toDirectoryId?:string,
 }
