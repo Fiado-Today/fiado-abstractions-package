cf6eba234364d8ecb5df33d3d972a2977723a292
jose-fiado <jma@somosfiado.com>
2023-11-28 14:55:13 -0500

    Actualizacion CardInformationItem

--- src/Fiado/Backoffice/Card/CardInformationOutput.ts
+++ src/Fiado/Backoffice/Card/CardInformationOutput.ts
@@ -1,15 +1,22 @@
 import {BankAccountCardStatus} from "../../../BankAccount/enums/BankAccountCardStatus";
+import {AddressOutput} from "../../Address/AddressOutput";
 
 export interface CardInformationItem {
     id: string;
     externalCardId: string;
     panLastDigits: string;
-    ternPanLastDigits: string;
+    externalPanLastDigits: string;
     fiadoHolderName: string;
     externalHolderName: string;
     typeOfCardId: string;
     fiadoStatus: BankAccountCardStatus,
     externalStatus: BankAccountCardStatus,
+    fiadoShippingAddress: AddressOutput,
+    externalShippingAddress: AddressOutput,
+    fiadoCreateDate: string;
+    externalCreateDate: string;
+    activationDate: string;
+    updateDate: string;
 }
 
 export interface CardInformationOutput {
